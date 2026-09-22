e590e3a9e22bca813c00f31b7dac9ad274dfa651
RonaldoLopes <ronaldorclopes@gmail.com>
2026-09-22 19:55:10 +0000

    criando estrutura databricks

---
+++ notebooks/01_coleta_apis.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Reading APIs for Auto Loader incremental ingestion
+
+# COMMAND ----------
+
+# DBTITLE 1,Imports
+import requests, json
+from datetime import date
+
+# COMMAND ----------
+
+# DBTITLE 1,Colect data
+def coletar_bcb_sgs(codigo_serie: int, nome: str):
+    url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo_serie}/dados/ultimos/30?formato=json"
+    resp = requests.get(url, timeout=30)
+    resp.raise_for_status()
+    dados = resp.json()
+    hoje = date.today()
+    caminho = (f"/Volumes/fininsight_dev/landing/raw/bcb/{nome}/"f"ano={hoje.year}/mes={hoje.month:02d}/dia={hoje.day:02d}/dados.json")
+    dbutils.fs.put(caminho, json.dumps(dados), overwrite=True)
+    
+coletar_bcb_sgs(11, "selic") # taxa Selic diária
+coletar_bcb_sgs(433, "ipca") # IPCA mensal
+coletar_bcb_sgs(1, "ptax_dolar") # câmbio PTAX
