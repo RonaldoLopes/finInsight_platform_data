9712a4228d00a06879f1d432abdecc159b27e66f
RonaldoLopes <ronaldorclopes@gmail.com>
2026-09-23 19:22:22 +0000

    .

--- notebooks/01_coleta_apis.py
+++ notebooks/01_coleta_apis.py
@@ -12,12 +12,13 @@ from datetime import date
 
 # DBTITLE 1,Colect data
 def coletar_bcb_sgs(codigo_serie: int, nome: str):
-    url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo_serie}/dados/ultimos/30?formato=json"
+    url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo_serie}/dados/ultimos/20?formato=json"
     resp = requests.get(url, timeout=30)
     resp.raise_for_status()
     dados = resp.json()
     hoje = date.today()
-    caminho = (f"/Volumes/fininsight_dev/landing/raw/bcb/{nome}/"f"ano={hoje.year}/mes={hoje.month:02d}/dia={hoje.day:02d}/dados.json")
+    caminho = (f"/Volumes/fininsight_dev/landing/raw/bcb/{nome}/"
+               f"ano={hoje.year}/mes={hoje.month:02d}/dia={hoje.day:02d}/dados.json")
     dbutils.fs.put(caminho, json.dumps(dados), overwrite=True)
     
 coletar_bcb_sgs(11, "selic") # taxa Selic diária
